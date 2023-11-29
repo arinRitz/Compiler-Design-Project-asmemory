2f8310804d64adbee9f56854427feab194d49dbc
Ahsan Raza Kolachi <Ahsan Raza Kolachi@DESKTOP-461QN7G>
2023-11-29 22:15:13 +0500

    Add project files.

---
+++ sir hidayat projets/Form1.Designer.vb
@@ -0,0 +1,208 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        MenuStrip1 = New MenuStrip()
+        FileToolStripMenuItem = New ToolStripMenuItem()
+        NewToolStripMenuItem = New ToolStripMenuItem()
+        OpenToolStripMenuItem = New ToolStripMenuItem()
+        SaveToolStripMenuItem = New ToolStripMenuItem()
+        SaveAsToolStripMenuItem = New ToolStripMenuItem()
+        ExitToolStripMenuItem = New ToolStripMenuItem()
+        EditToolStripMenuItem = New ToolStripMenuItem()
+        CopyToolStripMenuItem = New ToolStripMenuItem()
+        CutToolStripMenuItem = New ToolStripMenuItem()
+        PasteToolStripMenuItem = New ToolStripMenuItem()
+        ViewToolStripMenuItem = New ToolStripMenuItem()
+        ZoomInToolStripMenuItem = New ToolStripMenuItem()
+        ZoomOutToolStripMenuItem = New ToolStripMenuItem()
+        RuToolStripMenuItem = New ToolStripMenuItem()
+        CompileAndRunToolStripMenuItem = New ToolStripMenuItem()
+        HelpToolStripMenuItem = New ToolStripMenuItem()
+        AboutUsToolStripMenuItem = New ToolStripMenuItem()
+        RichTextBox1 = New RichTextBox()
+        MenuStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Items.AddRange(New ToolStripItem() {FileToolStripMenuItem, EditToolStripMenuItem, ViewToolStripMenuItem, RuToolStripMenuItem, HelpToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(757, 24)
+        MenuStrip1.TabIndex = 0
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' FileToolStripMenuItem
+        ' 
+        FileToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {NewToolStripMenuItem, OpenToolStripMenuItem, SaveToolStripMenuItem, SaveAsToolStripMenuItem, ExitToolStripMenuItem})
+        FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        FileToolStripMenuItem.Size = New Size(37, 20)
+        FileToolStripMenuItem.Text = "File"
+        ' 
+        ' NewToolStripMenuItem
+        ' 
+        NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        NewToolStripMenuItem.Size = New Size(112, 22)
+        NewToolStripMenuItem.Text = "New"
+        ' 
+        ' OpenToolStripMenuItem
+        ' 
+        OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        OpenToolStripMenuItem.Size = New Size(112, 22)
+        OpenToolStripMenuItem.Text = "Open"
+        ' 
+        ' SaveToolStripMenuItem
+        ' 
+        SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        SaveToolStripMenuItem.Size = New Size(112, 22)
+        SaveToolStripMenuItem.Text = "Save"
+        ' 
+        ' SaveAsToolStripMenuItem
+        ' 
+        SaveAsToolStripMenuItem.Name = "SaveAsToolStripMenuItem"
+        SaveAsToolStripMenuItem.Size = New Size(112, 22)
+        SaveAsToolStripMenuItem.Text = "Save as"
+        ' 
+        ' ExitToolStripMenuItem
+        ' 
+        ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        ExitToolStripMenuItem.Size = New Size(112, 22)
+        ExitToolStripMenuItem.Text = "Exit"
+        ' 
+        ' EditToolStripMenuItem
+        ' 
+        EditToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {CopyToolStripMenuItem, CutToolStripMenuItem, PasteToolStripMenuItem})
+        EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        EditToolStripMenuItem.Size = New Size(39, 20)
+        EditToolStripMenuItem.Text = "Edit"
+        ' 
+        ' CopyToolStripMenuItem
+        ' 
+        CopyToolStripMenuItem.Name = "CopyToolStripMenuItem"
+        CopyToolStripMenuItem.Size = New Size(102, 22)
+        CopyToolStripMenuItem.Text = "Copy"
+        ' 
+        ' CutToolStripMenuItem
+        ' 
+        CutToolStripMenuItem.Name = "CutToolStripMenuItem"
+        CutToolStripMenuItem.Size = New Size(102, 22)
+        CutToolStripMenuItem.Text = "Cut"
+        ' 
+        ' PasteToolStripMenuItem
+        ' 
+        PasteToolStripMenuItem.Name = "PasteToolStripMenuItem"
+        PasteToolStripMenuItem.Size = New Size(102, 22)
+        PasteToolStripMenuItem.Text = "Paste"
+        ' 
+        ' ViewToolStripMenuItem
+        ' 
+        ViewToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {ZoomInToolStripMenuItem, ZoomOutToolStripMenuItem})
+        ViewToolStripMenuItem.Name = "ViewToolStripMenuItem"
+        ViewToolStripMenuItem.Size = New Size(44, 20)
+        ViewToolStripMenuItem.Text = "View"
+        ' 
+        ' ZoomInToolStripMenuItem
+        ' 
+        ZoomInToolStripMenuItem.Name = "ZoomInToolStripMenuItem"
+        ZoomInToolStripMenuItem.Size = New Size(129, 22)
+        ZoomInToolStripMenuItem.Text = "Zoom In"
+        ' 
+        ' ZoomOutToolStripMenuItem
+        ' 
+        ZoomOutToolStripMenuItem.Name = "ZoomOutToolStripMenuItem"
+        ZoomOutToolStripMenuItem.Size = New Size(129, 22)
+        ZoomOutToolStripMenuItem.Text = "Zoom Out"
+        ' 
+        ' RuToolStripMenuItem
+        ' 
+        RuToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {CompileAndRunToolStripMenuItem})
+        RuToolStripMenuItem.Name = "RuToolStripMenuItem"
+        RuToolStripMenuItem.Size = New Size(40, 20)
+        RuToolStripMenuItem.Text = "Run"
+        ' 
+        ' CompileAndRunToolStripMenuItem
+        ' 
+        CompileAndRunToolStripMenuItem.Name = "CompileAndRunToolStripMenuItem"
+        CompileAndRunToolStripMenuItem.Size = New Size(166, 22)
+        CompileAndRunToolStripMenuItem.Text = "Compile and Run"
+        ' 
+        ' HelpToolStripMenuItem
+        ' 
+        HelpToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {AboutUsToolStripMenuItem})
+        HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        HelpToolStripMenuItem.Size = New Size(44, 20)
+        HelpToolStripMenuItem.Text = "Help"
+        ' 
+        ' AboutUsToolStripMenuItem
+        ' 
+        AboutUsToolStripMenuItem.Name = "AboutUsToolStripMenuItem"
+        AboutUsToolStripMenuItem.Size = New Size(180, 22)
+        AboutUsToolStripMenuItem.Text = "About us "
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.Font = New Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        RichTextBox1.Location = New Point(6, 27)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.Size = New Size(751, 362)
+        RichTextBox1.TabIndex = 1
+        RichTextBox1.Text = ""
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(757, 390)
+        Controls.Add(RichTextBox1)
+        Controls.Add(MenuStrip1)
+        FormBorderStyle = FormBorderStyle.FixedToolWindow
+        MainMenuStrip = MenuStrip1
+        Name = "Form1"
+        Text = "for memory!!"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ViewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveAsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CopyToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PasteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ZoomInToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ZoomOutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RuToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CompileAndRunToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutUsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
---
+++ sir hidayat projets/Form1.vb
@@ -0,0 +1,161 @@
+﻿Imports System.IO
+Public Class Form1
+    ' Variable to store the current file path
+    Private currentFilePath As String = ""
+    ' Variable to store the current zoom factor
+    Private currentZoomFactor As Single = 1.0
+
+
+    Private Sub MenuStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles MenuStrip1.ItemClicked
+
+    End Sub
+
+    Private Sub CompileAndRunToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CompileAndRunToolStripMenuItem.Click
+        Try
+            Dim process As New Process()
+            process.StartInfo.FileName = "python.exe"
+            process.StartInfo.Arguments = $"-c ""{RichTextBox1.Text}"""
+            process.StartInfo.UseShellExecute = False
+            process.StartInfo.RedirectStandardOutput = True
+            process.StartInfo.RedirectStandardError = True
+            process.StartInfo.CreateNoWindow = True
+            process.Start()
+
+            ' Read the output and error streams
+            Dim output As String = process.StandardOutput.ReadToEnd()
+            Dim errors As String = process.StandardError.ReadToEnd()
+
+            ' Display the output and errors in a MessageBox
+            If String.IsNullOrEmpty(errors) Then
+                MessageBox.Show(output, "Output")
+            Else
+                MessageBox.Show(errors, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+
+            process.WaitForExit()
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub CopyToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopyToolStripMenuItem.Click
+        If RichTextBox1.SelectionLength > 0 Then
+            RichTextBox1.Copy()
+        End If
+    End Sub
+
+    Private Sub CutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CutToolStripMenuItem.Click
+        If RichTextBox1.SelectionLength > 0 Then
+            RichTextBox1.Cut()
+        End If
+    End Sub
+
+
+
+    Private Sub PasteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PasteToolStripMenuItem.Click
+        RichTextBox1.Paste()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        SaveFile()
+    End Sub
+
+    Private Sub SaveAsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveAsToolStripMenuItem.Click
+        SaveFileAs()
+    End Sub
+    Private Sub SaveFile()
+        If String.IsNullOrEmpty(currentFilePath) Then
+            ' If the file has not been saved yet, use SaveFileAs
+            SaveFileAs()
+        Else
+            ' Save the file using the current file path
+            File.WriteAllText(currentFilePath, RichTextBox1.Text)
+        End If
+    End Sub
+
+    Private Sub SaveFileAs()
+        Dim saveFileDialog As New SaveFileDialog()
+        saveFileDialog.Filter = "Text Files|*.txt|All Files|*.*"
+
+        If saveFileDialog.ShowDialog() = DialogResult.OK Then
+            ' Save the file using the chosen file path
+            File.WriteAllText(saveFileDialog.FileName, RichTextBox1.Text)
+            currentFilePath = saveFileDialog.FileName
+        End If
+    End Sub
+
+    Private Sub ZoomInToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ZoomInToolStripMenuItem.Click
+        ZoomIn()
+    End Sub
+
+    Private Sub ZoomOutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ZoomOutToolStripMenuItem.Click
+        Zoomout()
+    End Sub
+    Private Sub ZoomIn()
+        ' Increase the zoom factor by 0.1 (adjust as needed)
+        currentZoomFactor += 0.1
+        ApplyZoom()
+    End Sub
+
+    Private Sub ZoomOut()
+        ' Decrease the zoom factor by 0.1 (adjust as needed)
+        currentZoomFactor -= 0.1
+        ApplyZoom()
+    End Sub
+
+    Private Sub ApplyZoom()
+        ' Set the ZoomFactor property of the RichTextBox
+        RichTextBox1.ZoomFactor = currentZoomFactor
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        End
+    End Sub
+
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        RichTextBox1.Text = ""
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        If PromptToSaveChanges() Then
+            Dim openFileDialog As New OpenFileDialog()
+            openFileDialog.Filter = "Text Files|*.txt|All Files|*.*"
+
+            If openFileDialog.ShowDialog() = DialogResult.OK Then
+                RichTextBox1.Text = File.ReadAllText(openFileDialog.FileName)
+                currentFilePath = openFileDialog.FileName
+            End If
+        End If
+    End Sub
+
+    ' ...
+
+    Private Function PromptToSaveChanges() As Boolean
+        If RichTextBox1.Modified Then
+            Dim result As DialogResult = MessageBox.Show("Do you want to save changes?", "Save Changes", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question)
+
+            If result = DialogResult.Yes Then
+                SaveFile() ' Save the changes
+                Return True
+            ElseIf result = DialogResult.No Then
+                Return True ' Continue without saving
+            Else
+                Return False ' Cancel the operation
+            End If
+        End If
+
+        Return True
+    End Function
+
+    Private Sub AboutUsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutUsToolStripMenuItem.Click
+        MessageBox.Show("Ahsan Raza, the group leader, along with team members Muhammad Shafiullah, Zoha Batool, and Urooj Fatima, are working on a collaborative project for their BS-CS III. The project involves designing and implementing a basic compiler, But They Created a Python IDE Instead. The goal is to create a functional compiler for a simple programming language.", "About US", MessageBoxButtons.OK, MessageBoxIcon.None)
+    End Sub
+End Class
+
+
+
+
